a16d8142be732dfcc8ff5752118b027ab793d277
JOSE MARCOS <joseluismarcosramos@outlook.com>
2023-02-17 14:40:37 -0500

    El Head

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,11 +5,17 @@ import Nosotros from '@/components/Nosotros'
 import Actividades from '@/components/Actividades'
 import Voluntarios from '@/components/Voluntarios'
 import Contacto from '@/components/Contacto'
+import Head from 'next/head'
 
 export default function Home () {
   return (
 
     <>
+      <Head>
+        <title>Ica Young</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
       <div className=''>
         <Header/>
