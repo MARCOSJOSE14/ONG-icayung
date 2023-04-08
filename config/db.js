49d6ec18cdfe072871ab590b94aeed3c62490a6d
JOSE MARCOS <joseluismarcosramos@outlook.com>
2023-04-08 14:56:46 -0500

    Base para SSG

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 import { createPool } from 'mysql2/promise'
 
 const pool = createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
+  host: 'colegio1.c0zyii5nlweg.sa-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Qubittos=2017',
   database: 'icayoung',
   port: 3306
 })
